f04bd623792e86f23abcc3506b4b57d2558a9b51
nourawh <nourasaadwh@gmail.com>
2025-11-04 18:09:59 +0300

    Noura Alwahabi answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,10 @@
--- ADD YOUR SQL QUERY HERE
+SELECT 
+    it.investor_id,
+    s.sector_name,
+    ROUND(
+        (CAST(it.no_of_shares AS FLOAT) / SUM(it.no_of_shares) OVER (PARTITION BY it.investor_id)) * 100,
+        2
+    ) AS share_percentage
+FROM investor_transactions it
+JOIN sectors s ON it.sector_id = s.sector_id
+ORDER BY it.investor_id, share_percentage DESC;
